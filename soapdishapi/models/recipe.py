79f916b2240d1ea5b4875511723397b9ed674719
Eric Frey <ericlfrey@gmail.com>
2023-06-05 13:31:36 -0500

    Ran Migrations. Cross fingers that everything is ok

--- soapdishapi/models/recipe.py
+++ soapdishapi/models/recipe.py
@@ -5,10 +5,10 @@ class Recipe(models.Model):
 
     maker = models.ForeignKey(
         "Soaper", on_delete=models.CASCADE, related_name='organized_events')
-    title = models.CharField(models.CharField(max_length=50))
+    title = models.CharField(max_length=50)
     water_amount = models.DecimalField(max_digits=5, decimal_places=3)
     lye_amount = models.DecimalField(max_digits=5, decimal_places=3)
     super_fat = models.DecimalField(max_digits=5, decimal_places=3)
-    description = models.CharField(models.CharField(max_length=100))
-    notes = models.CharField(models.CharField(max_length=200))
+    description = models.CharField(max_length=100)
+    notes = models.CharField(max_length=200)
     public = models.BooleanField(default=False)
